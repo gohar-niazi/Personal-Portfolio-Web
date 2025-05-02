c46a2a5a897f91a4fec0bd784bb0a3f9d3ef9292
gohar-niazi <goharniazi.dev@gmial.com>
2025-05-02 18:33:31 +0500

    Final Update

--- Main.js
+++ Main.js
@@ -1,7 +1,27 @@
-var typed = new Typed("#typed-text", {
+new Typed("#typed-text", {
   strings: ["Hi! I am Gohar Nawaz Niazi, a Passionate Web Developer."],
   typeSpeed: 50,
   backSpeed: 30,
   backDelay: 3000,
   loop: true,
+  showCursor: false,
+});
+
+// Success message
+const form = document.getElementById("contact-form");
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const formData = new FormData(form);
+  fetch(form.action, {
+    method: form.method,
+    body: formData,
+    headers: { Accept: "application/json" },
+  }).then((response) => {
+    if (response.ok) {
+      form.reset();
+      document.getElementById("thank-you-message").style.display = "block";
+    } else {
+      alert("Oops! Something went wrong.");
+    }
+  });
 });
