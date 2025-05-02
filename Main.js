f10f590ced4cb4c9502084ff2519aec740180adc
gohar-niazi <goharniazi.dev@gmial.com>
2025-05-02 17:40:42 +0500

    Final Update

---
+++ Main.js
@@ -0,0 +1,7 @@
+var typed = new Typed("#typed-text", {
+  strings: ["Hi! I am Gohar Nawaz Niazi, a Passionate Web Developer."],
+  typeSpeed: 50,
+  backSpeed: 30,
+  backDelay: 3000,
+  loop: true,
+});
